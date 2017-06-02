aa11425c8fff38e449cc17cca9a52d2c2762f259
Delink-D <delinkdesigns@gmail.com>
2017-06-02 09:57:15 +0300

    Adding change detection on the binary change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,10 @@ export class AppComponent {
 		});
 	}
 
+	bin = 0;
+	oct = 0;
+	hex = 0;
+
 	// listen for change on the decimal fild
 	decChange(oldVal, newVal){
 		if (newVal != "") {
@@ -29,6 +33,19 @@ export class AppComponent {
 			this.myForm.patchValue({binary: ""});
 			this.myForm.patchValue({octal: ""});
 			this.myForm.patchValue({hexa: ""});
+		}
+	}
+
+	// // listening to change on the binary fild
+	binChange(oldVal, newVal){
+		this.bin = this.bin + 1;
+		if (this.bin == 1) {
+			if (newVal != "") {
+				this.myForm.patchValue({decimal: parseInt(newVal, 2).toString(10)});
+			}else{
+				this.myForm.patchValue({decimal: ""});
+			}
+			this.bin = 0;
 		}
 	}
 }
